4643f8969608ad390b8ab7873ff9e84c686bb73e
Jimmy Jouanne <jimmy.jouanne@wttj.co>
2022-09-30 08:11:52 +0200

    chore: add svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /// <reference types="vitest" />
+/// <reference types="vite-plugin-svgr/client" />
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   test: {
     globals: true,
     environment: 'jsdom',
